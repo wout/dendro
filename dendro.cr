f0a382fa0392618c5f644ac47ac8eba43f1ca697
Wout <hi@wout.codes>
2022-02-16 18:44:37 +0100

    feat: add colors for coming weeks

--- dendro.cr
+++ dendro.cr
@@ -41,6 +41,18 @@ struct Dendro
     %(#DFEDAE), # 3: phosphor
     %(#DEDAF1), # 4: lilac
     %(#E5B7A5), # 5: copper
+    %(#ff6d6a), # 6: coral
+    %(#e8cc00), # 7: saffron
+    %(#82a3ff), # 8: cornflower
+    %(#ff8200), # 9: goldfish
+    %(#26323C), # 10: slate
+    %(#fc8eac), # 11: flamingo
+    %(#05c3dd), # 12: azure
+    %(#C71A2F), # 13: amaranth
+    %(#9063cd), # 14: royalpurple
+    %(#9DEE00), # 15: neongreen
+    %(#623412), # 16: chocolate
+    %(#fb48c4), # 17: neonpink
   }
 
   alias Point = Tuple(Float64, Float64)
--- playground.cr
+++ playground.cr
@@ -1,31 +1,28 @@
 require "./dendro"
 require "./helpers/util"
 
-# Set variables here
-add_core = true
-color_id = 0
-number = 1
-previous_cores = Util.js_to_json <<-JSON
-[
-   {
-      age: 1,
-      cp: [
-         48,
-         11
-      ],
-      life: 21,
-      max_r: 8
-   }
-]
-JSON
+[10].each do |i|
+  # Set variables here
+  add_core = true
+  color_id = i
+  number = i
+  previous_cores = Util.js_to_json <<-JSON
+   [{"cp": [82, 19], "age": 21, "life": 22, "max_r": 6},
+    {"cp": [20, 46], "age": 20, "life": 22, "max_r": 6},
+    {"cp": [99, 46], "age": 18, "life": 23, "max_r": 8},
+    {"cp": [47, 63], "age": 7, "life": 22, "max_r": 5},
+    {"cp": [48, 80], "age": 6, "life": 19, "max_r": 5},
+    {"cp": [22, 73], "age": 3, "life": 19, "max_r": 6}]
+   JSON
 
-# This part builds the Dendro
-dendro = Dendro.new(
-  seed: number,
-  color: color_id,
-  previous_cores: previous_cores,
-  add_core: add_core
-)
+  # This part builds the Dendro
+  dendro = Dendro.new(
+    seed: number,
+    color: color_id,
+    previous_cores: previous_cores,
+    add_core: add_core
+  )
 
-# This part writes the resulting SVG file to the 'playground' folder
-File.write(Util.svg_file_name(number), dendro.to_svg)
+  # This part writes the resulting SVG file to the 'playground' folder
+  File.write(Util.svg_file_name(number), dendro.to_svg)
+end
