61d6bef3645c56ead2da565bc00541eb19c29203
Wout <hi@wout.codes>
2022-02-16 16:45:15 +0100

    feat: add option for multi-coloured Dendros

--- playground.cr
+++ playground.cr
@@ -1,4 +1,4 @@
-require "./dendro"
+require "./dendro_multi_era"
 require "./helpers/util"
 
 # Set variables here
@@ -6,17 +6,12 @@ add_core = true
 color_id = 0
 number = 1
 previous_cores = Util.js_to_json <<-JSON
-[
-   {
-      age: 1,
-      cp: [
-         48,
-         11
-      ],
-      life: 21,
-      max_r: 8
-   }
-]
+[{"cp": [43, 9], "age": 17, "life": 24, "max_r": 8},
+ {"cp": [33, 37], "age": 15, "life": 23, "max_r": 6},
+ {"cp": [15, 18], "age": 14, "life": 15, "max_r": 6},
+ {"cp": [91, 57], "age": 9, "life": 25, "max_r": 5},
+ {"cp": [56, 39], "age": 5, "life": 17, "max_r": 8},
+ {"cp": [59, 68], "age": 4, "life": 18, "max_r": 5}]
 JSON
 
 # This part builds the Dendro
